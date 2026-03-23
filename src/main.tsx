df33476954f9f710ffff5629287695270fb3ebf7
eric hartung <ehartung606@gmail.com>
2026-03-23 15:58:40 -0500

    add inventory system

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
 import Payroll from './pages/Payroll.tsx'
+import Inventory from './pages/Inventory.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/payroll" element={<Payroll />} />
+        <Route path="/inventory" element={<Inventory />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
